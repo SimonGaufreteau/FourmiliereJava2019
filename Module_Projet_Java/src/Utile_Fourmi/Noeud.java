09d4ef78c00b59dc8e867398a039f6be9266faf9
Thomas <thomas.ninet@free.fr>
2019-12-12 11:02:59 +0100

    Création de la classe Noeud

---
+++ Module_Projet_Java/src/Utile_Fourmi/Noeud.java
@@ -0,0 +1,13 @@
+package Utile_Fourmi;
+
+public class Noeud {
+    protected String text;
+
+    public Noeud(String text) {
+        this.text = text;
+    }
+
+    public String getText () {
+        return text;
+    }
+}
