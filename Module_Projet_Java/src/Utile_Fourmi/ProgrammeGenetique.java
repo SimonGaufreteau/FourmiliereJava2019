dc5c9d2be9be5c6272b12b65515eb86ad5ea3957
Thomas <thomas.ninet@free.fr>
2019-12-22 16:05:14 +0100

    Test

--- Module_Projet_Java/src/Utile_Fourmi/ProgrammeGenetique.java
+++ Module_Projet_Java/src/Utile_Fourmi/ProgrammeGenetique.java
@@ -12,7 +12,6 @@ public class ProgrammeGenetique implements  Serializable, Cloneable {
     private Noeud valeur;
     private ProgrammeGenetique aGauche;
     private ProgrammeGenetique aDroite;
-    //TEST
 
     // Constructeur avec seulement une valeur (sans fils)
     public ProgrammeGenetique(Noeud valeur){
